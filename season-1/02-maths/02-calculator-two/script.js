85b976ceb73b51b2c86b867962a3bdad6080ff4c
Axel Siriez <axel.siriez@gmail.com>
2022-02-28 15:45:28 +0100

    02-calculator-two

--- season-1/02-maths/02-calculator-two/script.js
+++ season-1/02-maths/02-calculator-two/script.js
@@ -3,9 +3,35 @@
 
 (() => {
     // to get the value of an input: document.getElementById("element-id").value
+    var op1 = document.getElementById("op-one");
+    var op2 = document.getElementById("op-two");
+    var result = 0;
 
     const performOperation = operation => {
-        // perform the operation
+        switch(operation) {
+            case "addition":
+                result = parseInt(op1.value) + parseInt(op2.value);
+                alert(result);
+
+            break;
+            case "substraction":
+                result = parseInt(op1.value) - parseInt(op2.value);
+                alert(result);
+
+            break
+            case "multiplication":
+                result = parseInt(op1.value) * parseInt(op2.value)
+                alert(result)
+
+            break
+            case "division":
+                result = parseInt(op1.value) / parseInt(op2.value)
+                alert(result)
+
+            break 
+            default :    
+                alert("choose an operation")
+        }
     };
 
     Array.from(document.querySelectorAll("button.operator")).forEach($btn =>
