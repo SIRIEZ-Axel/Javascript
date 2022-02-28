777df30d8b5a8a97429f8d45aa7e1cb231758d5e
Axel Siriez <axel.siriez@gmail.com>
2022-02-28 11:09:37 +0100

    var prompt

--- season-1/01-base/02-var-prompt/script.js
+++ season-1/01-base/02-var-prompt/script.js
@@ -2,10 +2,11 @@
 // 01-base/02-var-prompt/script.js - 1.2: var & prompt
 
 (() => {
-    // Don't bother with that, look at your browser's console(chrome), you can delete it afterwards ;)
-    console.log('%c ', 'padding: 38px; background:url(https://becode.org/app/uploads/2020/03/bc_mailsign_seal.png) no-repeat;');
-    console.log('Hello Keller programmer');
-
     // YOUR CODE HERE
+    let hello = "hello, ";
+    let name = prompt("give me your name: ");
+
+    window.alert(hello + name);
+
 })();
 
