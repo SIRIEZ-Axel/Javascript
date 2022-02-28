386c7d055bb58e836c3f78fdef29be9c70470ca7
Axel Siriez <axel.siriez@gmail.com>
2022-02-28 14:09:15 +0100

    01-calculator-one

--- season-1/02-maths/01-calculator-one/script.js
+++ season-1/02-maths/01-calculator-one/script.js
@@ -3,20 +3,29 @@
 
 (() => {
     // to get the value of an input: document.getElementById("element-id").value
+    var op1 = document.getElementById("op-one")
+    var op2 = document.getElementById("op-two")
+    var result = 0;
 
     document.getElementById("addition").addEventListener("click", function() {
-        // perform an addition
+        result = parseInt(op1.value) + parseInt(op2.value);
+        alert(result)
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
-        // perform an substraction
+        result = parseInt(op1.value) - parseInt(op2.value);
+        alert(result)
+
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
-        // perform an multiplication
+        result = parseInt(op1.value) * parseInt(op2.value);
+        alert(result)
+
     });
 
-    document.getElementById("division").addEventListener("click", function() {
-        // perform an division
+    document.getElementById("division").addEventListener("click", function() {    
+        result = parseInt(op1.value) / parseInt(op2.value);
+        alert(result)
     });
 })();
