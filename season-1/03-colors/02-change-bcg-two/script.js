91ef75c83fb062a7294de5be8dad458fcc9cce78
Axel Siriez <axel.siriez@gmail.com>
2022-03-01 12:25:39 +0100

    02 change bcg two

--- season-1/03-colors/02-change-bcg-two/script.js
+++ season-1/03-colors/02-change-bcg-two/script.js
@@ -2,7 +2,15 @@
 // 03-colors/02-change-bcg-two/script.js - 3.2: Bcg 2
 
 (() => {
+    var btn = document.getElementById("run");
+    
+    function changeColor() {
+        var bcg = document.getElementById("color").value;
+        document.body.style.backgroundColor = bcg;
+    }
 
-    // your code here
+    btn.addEventListener("click", () => {
+        changeColor();
+    })
 
 })();
