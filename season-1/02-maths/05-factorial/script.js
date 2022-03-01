683338d2c738ece2c208cab0c03439e2eb31605a
Axel Siriez <axel.siriez@gmail.com>
2022-03-01 10:46:03 +0100

    05-factorial

--- season-1/02-maths/05-factorial/script.js
+++ season-1/02-maths/05-factorial/script.js
@@ -6,8 +6,17 @@
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", () => {
+    var n = document.getElementById("number").value;
+    
+    function factorial(n){
+        if(n == 0 || n == 1){
+            return 1;
+        }else{
+            return n * factorial(n-1);
+        }
+    }
 
-        // your code here
+    alert("The factorial of " + n + " is " + factorial(n))
 
     });
 
