1399ca8b0f31e28896e0ab42ce4736d40367dc98
Axel Siriez <axel.siriez@gmail.com>
2022-03-01 11:23:45 +0100

    01 change bcg one

--- season-1/03-colors/01-change-bcg-one/script.js
+++ season-1/03-colors/01-change-bcg-one/script.js
@@ -1,9 +1,27 @@
-
 // 03-colors/01-change-bcg-one/script.js - 3.1: Bcg one (1)
 
 
 (() => {
 
-    // your code here
+    document.getElementById("red");
+    red.addEventListener("click", () => {
+        document.body.style.backgroundColor = 'red';
+    })
+
+    document.getElementById("green");
+    green.addEventListener("click", () => {
+        document.body.style.backgroundColor = 'green';
+    })
+
+    document.getElementById("yellow");
+    yellow.addEventListener("click", () => {
+        document.body.style.backgroundColor = 'yellow';
+    })
+
+    document.getElementById("blue");
+    blue.addEventListener("click", () => {
+        document.body.style.backgroundColor = 'blue';
+    })
+    
 
 })();
