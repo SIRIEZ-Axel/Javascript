187d9325afe755abb6c528562b9bf46dfb010764
Axel Siriez <axel.siriez@gmail.com>
2022-03-01 12:20:38 +0100

    03 change bcg three

--- season-1/03-colors/03-change-bcg-three/script.js
+++ season-1/03-colors/03-change-bcg-three/script.js
@@ -4,6 +4,20 @@
 
 (() => {
 
-    // your code here
+    let btn = document.getElementById("run");
+
+    function random_bg_color() {
+        var x = Math.floor(Math.random() * 256);
+        var y = Math.floor(Math.random() * 256);
+        var z = Math.floor(Math.random() * 256);
+        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+        
+        document.body.style.background = bgColor;
+    }
+    
+    btn.addEventListener("click", () => {
+        document.body.style.backgroundColor = random_bg_color();
+    })
+
 
 })();
