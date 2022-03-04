b08e6a100b4cc3d31c38cc4f07ab57249c78114b
Axel Siriez <axel.siriez@gmail.com>
2022-03-04 10:29:58 +0100

    04 getter setter

--- season-1/07-classes/04-getter-setter/script.js
+++ season-1/07-classes/04-getter-setter/script.js
@@ -2,5 +2,29 @@
 
 
 (() => {
-    // your code here
+    class person{
+        constructor(firstname, lastname){
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+        set firstname(name) {
+            this.first = name;
+        }
+
+        set lastname(name) {
+            this.last = name;
+        }
+
+        get name() {
+            return this.first +" "+ this.last;
+        }
+    }
+
+    var btn = document.getElementById("run")
+    btn.addEventListener("click", () => {
+
+        let person1 = new person("Axel", "siriez");
+        
+        console.log(person1.name)
+    })
 })();
