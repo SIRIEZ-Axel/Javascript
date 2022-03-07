a8709ee28f92e2f1a19dac542e1d7bd46bcb1043
Axel Siriez <axel.siriez@gmail.com>
2022-03-07 13:28:32 +0100

    12 change event input two

--- season-1/08-dom/12-change-event-input-two/script.js
+++ season-1/08-dom/12-change-event-input-two/script.js
@@ -4,5 +4,19 @@
 (() => {
 
     // your code here
+    var pass1 = document.getElementById("pass-one")
+
+    var valider = document.getElementById("validity")
+
+    var passw= /^[^.*00$]\w{0,6}$/;
+
+    pass1.onkeyup = function () {
+     
+        if(pass1.value.match(passw)) {
+            valider.innerHTML = "Not ok";
+        } else {
+            valider.innerHTML = "Ok";
+        }
+    }
 
 })();
