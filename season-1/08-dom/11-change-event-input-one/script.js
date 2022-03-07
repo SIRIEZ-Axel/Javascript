adabac4ed9a22c2a46dbca0926be003f75c46571
Axel Siriez <axel.siriez@gmail.com>
2022-03-07 12:09:11 +0100

    11 change event input one

--- season-1/08-dom/11-change-event-input-one/script.js
+++ season-1/08-dom/11-change-event-input-one/script.js
@@ -4,5 +4,17 @@
 (() => {
 
     // your code here
+    var pass1 = document.getElementById("pass-one")
+
+    var counter = document.getElementById("counter")
+
+    pass1.onkeyup = function () {
+        counter.innerHTML = (this.value.length) + "/10"; 
+        var max_chars = 9;
+
+        if(pass1.value.length > max_chars) {
+            pass1.value = pass1.value.substr(0, max_chars);
+        }
+    }
 
 })();
