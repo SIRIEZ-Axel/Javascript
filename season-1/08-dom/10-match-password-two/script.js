d43b345ffe28d6ae7515ddd4c0bd391cb7116e48
Axel Siriez <axel.siriez@gmail.com>
2022-03-07 11:51:22 +0100

    10 match password two

--- season-1/08-dom/10-match-password-two/script.js
+++ season-1/08-dom/10-match-password-two/script.js
@@ -4,5 +4,24 @@
 (() => {
 
     // your code here
+    var btn = document.getElementById("run")
+
+    var pass1 = document.getElementById("pass-one")
+
+    var pass2 = document.getElementById("pass-two")
+
+
+    btn.addEventListener("click", () => {
+        
+        if(pass1.value !== pass2.value) {
+            pass2.style.border = ".1rem solid red"
+            pass1.style.border = ".1rem solid red"
+            pass1.classList.add('error')
+            pass2.classList.add('error')
+        } else{
+            pass2.style.border = ".1rem solid green"
+            pass1.style.border = ".1rem solid green"
+        }
+    })
 
 })();
