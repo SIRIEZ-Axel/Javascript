29591d3048388f59b0e26619cd9f3ad31ff1624b
Axel Siriez <axel.siriez@gmail.com>
2022-03-07 11:20:14 +0100

    09 match password

--- season-1/08-dom/09-match-password-one/script.js
+++ season-1/08-dom/09-match-password-one/script.js
@@ -4,5 +4,22 @@
 (() => {
 
     // your code here
+    var btn = document.getElementById("run")
+
+    var pass1 = document.getElementById("pass-one")
+
+    var pass2 = document.getElementById("pass-two")
+
+    btn.addEventListener("click", () => {
+        
+        if(pass1.value !== pass2.value) {
+            pass2.style.border = ".1rem solid red"
+            pass1.style.border = ".1rem solid red"
+        } else{
+            pass2.style.border = ".1rem solid green"
+            pass1.style.border = ".1rem solid green"
+        }
+    })
+
 
 })();
